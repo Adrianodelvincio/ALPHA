66bb9a69eed5e8f8e09a315064c9d4c3d66e31a5
Adrianodelvincio <eavinci@outlook.it>
2023-11-16 12:03:43 +0100

    remove shared libraries from tracking

--- ALPHA-2/AutoDict_MontecarloForFit.cxx
+++
@@ -1,7 +0,0 @@
-#include "Headers/MontecarloForFit.h"
-#ifdef __CLING__ 
-#pragma link C++ nestedclasses;
-#pragma link C++ nestedtypedefs;
-#pragma link C++ class MontecarloForFit+;
-#pragma link C++ class MontecarloForFit::*+;
-#endif
