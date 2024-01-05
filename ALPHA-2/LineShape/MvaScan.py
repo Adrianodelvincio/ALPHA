1a0124e4a387877f5e1b83a8bad7d76199ca00d8
Adrianodelvincio <eavinci@outlook.it>
2024-01-05 19:27:16 +0100

    change on the office computer

--- ALPHA-2/LineShape/MvaScan.py
+++ ALPHA-2/LineShape/MvaScan.py
@@ -8,27 +8,46 @@ rate, efficiency = np.loadtxt("MVA-points.txt", unpack = True)
 # DATA GENERATION
 simulationCPP = "LoopLineShape"
 mvaScan = "\"true\""
-Nfiles = "2"
+Nfiles = "500"
 ConfFile = "\"ToyConfiguration.txt\""
 
 ROOT.gInterpreter.ProcessLine(".L LoopLineShape.cpp")
+"""
 for i, item in enumerate(efficiency):
 	CosmicRate = str(rate[i])
 	Efficiency = str(item)
 	folder = "mva_" + str(i)
-	# Create folder	
-	if not os.path.exists("/home/commodo98/Documenti/ALPHA/ALPHA-2/LineShape/" + folder):	# Check if the folder exist
-		os.makedirs("/home/commodo98/Documenti/ALPHA/ALPHA-2/LineShape/" + folder)			# Create the folder		
+	# Create folder
 	
+	if not os.path.exists("/home/adriano/Documents/ALPHA/ALPHA-2/LineShape/" + folder):	# Check if the folder exist
+		os.makedirs("/home/adriano/Documents/ALPHA/ALPHA-2/LineShape/" + folder)			# Create the folder
+
 	code = simulationCPP + "(" + Nfiles + ",\"" + folder + "/\"," + mvaScan + "," + CosmicRate + "," + Efficiency + "," + ConfFile + ")"
 	print("running: ", code)
 	# Execute simulation code
 	ROOT.gInterpreter.ProcessLine(code)
 	# copy the configuration file in the folder
 	os.popen("cp ToyConfiguration.txt " + folder + "/"  + ConfFile)
+"""
+for i in range(18,len(efficiency)):
+	CosmicRate = str(rate[i])
+	Efficiency = str(efficiency[i])
+	folder = "mva_" + str(i)
+	# Create folder
+	
+	if not os.path.exists("/home/adriano/Documents/ALPHA/ALPHA-2/LineShape/" + folder):	# Check if the folder exist
+		os.makedirs("/home/adriano/Documents/ALPHA/ALPHA-2/LineShape/" + folder)			# Create the folder
+
+	code = simulationCPP + "(" + Nfiles + ",\"" + folder + "/\"," + mvaScan + "," + CosmicRate + "," + Efficiency + "," + ConfFile + ")"
+	print("running: ", code)
+	# Execute simulation code
+	ROOT.gInterpreter.ProcessLine(code)
+	# copy the configuration file in the folder
+	os.popen("cp ToyConfiguration.txt " + folder + "/"  + ConfFile)	
 
 # DATA ANALYSIS
 # Execute analysis code
+"""
 ROOT.gInterpreter.ProcessLine(".L ScanAnalysis.cpp")
 for i, item in enumerate(rate):
 	analysisCPP = "ScanAnalysis"
@@ -39,3 +58,4 @@ for i, item in enumerate(rate):
 	#result = ROOT.std.vector("double")(10)
 	result = ROOT.gInterpreter.ProcessLine(analysisCPP + "(" + folder + "," + ConfFile + "," + listFile + "," + algorithmParameters + ")")
 	print("point %d" % i, " ", type(result), " ", result)
+"""
